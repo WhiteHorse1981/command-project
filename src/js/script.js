a869e27167bb53d4a62533c320fe973a7af7283c
Andrey Belokon <belokon1981andrey@gmail.com>
2022-10-17 18:13:54 +0300

    Финальные правки и добавление слайда в проект

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+$('.slider').slick({
+    arrows:true,
+    adaptiveHeight:true,
+    slidesShow:1,
+    infinite: false,
+    speed: 500,
+    autoplay:true,
+    autoplaySpeed:1500,
+    pauseOnFocus:true,
+    pauseOnHover:true,
+    pauseOnDotsHover:true,
+    draggable:false
+});
